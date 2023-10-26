41ab2534336200e1a932ae8f2e044cdbc8b0507b
LirySouza <lirianysouza15@gmail.com>
2023-10-26 08:29:49 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProjetoBiblioteca.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+    }
+
+}
