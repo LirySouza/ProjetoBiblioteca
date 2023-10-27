ce553de93713eb65a177484b36215052ad2cb17e
Manukefraus <manuu.kefraus@gmail.com>
2023-10-27 08:16:31 -0300

    Alterações feitas

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<Contexto> //Liriany
-   (options => options.UseSqlServer("Data Source=SB-1490643\\SQLSENAI;Initial Catalog = PojetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
+/*builder.Services.AddDbContext<Contexto> //Liriany
+   (options => options.UseSqlServer("Data Source=SB-1490643\\SQLSENAI;Initial Catalog = PojetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));*/
 
 builder.Services.AddDbContext<Contexto> //Manuela
    (options => options.UseSqlServer("Data Source=SB-1490633\\SQLSENAI;Initial Catalog = PojetoBiblioteca;Integrated Security = True;TrustServerCertificate = True"));
